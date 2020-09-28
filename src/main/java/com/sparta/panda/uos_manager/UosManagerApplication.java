ef33c441836eed714e488fc8786962c9f047e6c7
johndonnedavid <johndonne@hotmail.co.uk>
2020-09-28 15:03:51 +0100

    First commit

---
+++ src/main/java/com/sparta/panda/uos_manager/UosManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.panda.uos_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UosManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UosManagerApplication.class, args);
+    }
+
+}

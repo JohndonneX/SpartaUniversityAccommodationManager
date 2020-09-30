b88da8af7e2da3efd109b79f185a5223b3eb16af
Tasnia <tkhan2@spartaglobal.com>
2020-09-30 11:12:33 +0100

    Included issue form for resident and some footer cleanup and unused import statements deleted

--- src/main/java/com/sparta/panda/uos_manager/generalPublic/controllers/PublicController.java
+++ src/main/java/com/sparta/panda/uos_manager/generalPublic/controllers/PublicController.java
@@ -2,10 +2,6 @@ package com.sparta.panda.uos_manager.generalPublic.controllers;
 
 import com.sparta.panda.uos_manager.common.entities.Enquiry;
 import com.sparta.panda.uos_manager.generalPublic.services.EnquiryService;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,23 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
 import java.time.LocalDateTime;
 
 @Controller
 public class PublicController {
-    private EnquiryService enquiryService;
+    private final EnquiryService enquiryService;
 
     @Autowired
     public PublicController(EnquiryService enquiryService) {
